f97aa75e4d636f4003b41da7635530e56042e499
Chris Browne <yoda2031@gmail.com>
2012-12-15 19:12:44 +0100

    #3 maptile and zone work

--- client/include/Zone.hpp
+++ client/include/Zone.hpp
@@ -1,23 +1,27 @@
 #ifndef ZONE_HPP
 #define ZONE_HPP
 
+#include "CachedXML.hpp"
 #include "MapTile.hpp"
 
 namespace Citizens
 {
-	enum zone_scalar {
-		M1 = -1,
-		Z = 0,
-		P1 = 1
-	};
-	struct zone_coord
+	class zone_coord
 	{
-		zone_scalar x,y;
+	public:
+		zone_coord() { x = 0; y = 0; }
+		zone_coord(short int x,short int y)
+		{
+			this->x = (0 < x) - (x < 0);
+			this->y = (0 < y) - (y < 0);
+		}
+	private:
+		short int x,y;
 	};
 	class Zone
 	{
 	public:
-		void load(const irr::io::path& from_file);
+		void load(const CachedXML xml);
 		void unload(void);
 		MapTile* get_tile(zone_coord at);
 	private:
