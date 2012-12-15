4148d50f104269c85873c3a62c8f3afa7264f1a9
Chris Browne <yoda2031@gmail.com>
2012-12-15 19:23:29 +0100

    Deprecated much of Protocol

--- client/src/Protocol.cpp
+++ client/src/Protocol.cpp
@@ -1,16 +1,3 @@
 #include "Protocol.hpp"
 
 using namespace Citizens;
-
-std::map<std::string,command> Protocol::valid_commands = {
-		{"getpos",GETPOS}
-	};
-
-
-command Protocol::parse(const std::string& cmd) const
-{
-	if(valid_commands.count(cmd) > 0)
-		return valid_commands[cmd];
-	else
-		return INVALID;
-}
